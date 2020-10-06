26209af204811f686c9bbf3fc0491191b2b40e29
Louis Kirkham <louiskirkham@gmail.com>
2020-10-06 16:18:46 +0100

    added power saving mode

--- spec/ThermostatSpec.js
+++ spec/ThermostatSpec.js
@@ -1,5 +1,6 @@
 describe('Thermostat', function() {
 // Starting value temperature = 20
+  let thermostat
 
   beforeEach(function() {
     thermostat = new Thermostat();
@@ -7,38 +8,53 @@ describe('Thermostat', function() {
   })
   
   it('should have a start temperature value of', function () {
-    let thermostat = new Thermostat(); // 'let' because I don't want global variable
     expect(thermostat.temperature).toEqual(20);
   });
 
   it('should have a power saving mode', function () {
-    let thermostat = new Thermostat(); // 'let' because I don't want global variable
     expect(thermostat.powerSavingMode).toEqual(true);
   });
 
   describe('increase', function() {
-    let thermostat = new Thermostat();
     it('can increase the temperature', function() {
       thermostat.increase(5);
       expect(thermostat.temperature).toEqual(25);
     })
+    it('cannot increase above 25 in power saving mode', function() {
+      thermostat.increase(20);
+      expect(thermostat.temperature).toEqual(25);
+    })
+    it('cannot increase above 32 in non power saving mode', function() {
+      thermostat.switchMode();
+      thermostat.increase(20);
+      expect(thermostat.temperature).toEqual(32);
+    })
   })
 
   describe('decrease', function() {
-    let thermostat = new Thermostat();
     it('can decrease the temperature', function() {
       thermostat.decrease(5);
       expect(thermostat.temperature).toEqual(15);
     })
 
     it('can decrease temperature to minimum of 10 degrees', function(){
-      let thermostat = new Thermostat();
       thermostat.decrease(15);
       expect(thermostat.temperature).toEqual(10);
     })
   })
 
+  describe('switchMode', function() {
+    it('can turn the power mode on', function() {
+      thermostat.switchMode();
+      expect(thermostat.powerSavingMode).toEqual(false)
+    })
 
+    it('can turn the power mode off', function() {
+      thermostat.switchMode();
+      thermostat.switchMode();
+      expect(thermostat.powerSavingMode).toEqual(true)
+    })
+  })
 
 
 
--- src/Thermostat.js
+++ src/Thermostat.js
@@ -4,19 +4,29 @@ class Thermostat {
     this.powerSavingMode = true
   }
 
-  temperature() {
-    return this.temperature;
-  }
-  
-
   increase(value) {
     this.temperature += value
+    if(this.powerSavingMode == true && this.temperature > 25) {
+      this.temperature = 25
+    }
+    else if(this.powerSavingMode == false && this.temperature > 32) {
+      this.temperature = 32
+    }
   }
  
   decrease(value) {
     this.temperature -= value
     if(this.temperature < 10) {
     this.temperature = 10 } // resetting to ten
+  }
+
+  switchMode() {
+    if(this.powerSavingMode == true) {
+      this.powerSavingMode = false
+    }
+    else if(this.powerSavingMode == false) {
+      this.powerSavingMode = true
+    }
   }
 
 
