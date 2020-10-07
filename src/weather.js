c020364832e350860c50fb288d74e8fbf7ac0090
samcolson4 <samcolson4@gmail.com>
2020-10-07 16:40:31 +0100

    Added London weather to html page

---
+++ src/weather.js
@@ -0,0 +1,24 @@
+function weatherBalloon() {
+  var key = '79734182354473bd1378b3db74651f2d';
+  fetch('https://api.openweathermap.org/data/2.5/weather?id=' + '2643741' + '&appid=' + key)
+  .then(function(resp) { return resp.json() }) // Convert data to json
+  .then(function(data) {
+    drawWeather(data);
+  })
+  .catch(function() {
+    // catch any errors
+  });
+}
+
+window.onload = function() {
+  weatherBalloon( 6167865 );
+}
+
+function drawWeather( d ) {
+	var celcius = Math.round(parseFloat(d.main.temp)-273.15);
+	var fahrenheit = Math.round(((parseFloat(d.main.temp)-273.15)*1.8)+32); 
+	
+	document.getElementById('description').innerHTML = d.weather[0].description;
+	document.getElementById('temp').innerHTML = celcius + '&deg;';
+	document.getElementById('location').innerHTML = d.name;
+}
